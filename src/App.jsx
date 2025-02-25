23071c773f0adb7c5664f0e98856f6667460b6c5
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-02-25 18:54:59 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+
 function App() {
   const [num, setNum] = useState(0);
   return (
     <>
-      <p>{num}</p>
-      <button onClick={() => setNum(num + 1)}>+</button>
-      <button onClick={() => setNum(num - 1)}>-</button>
+      <div className="main-div">
+        <p>{num}</p>
+        <button onClick={() => setNum(num + 1)}>+</button>
+        <button onClick={() => setNum(num - 1)}>-</button>
+      </div>
     </>
   );
 }
